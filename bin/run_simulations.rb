fa7ef2eea79a974ade5b1d3f4063c460cb1d0ff7
Tom Calloway <tom@esparklearning.com>
2016-12-31 16:19:33 -0600

    add report/simulation runner; simplifications

---
+++ bin/run_simulations.rb
@@ -0,0 +1,18 @@
+require_relative '../lib/number_list'
+require_relative '../lib/ui/full_keypad.rb'
+
+number_list = NumberList.new("input_examples/2016-01-food-outside-pinch.csv")
+
+simulations = FullKeypad.run_simulations(number_list)
+
+simulations.each do |sim|
+  deviation = (sim.actual_sum - sim.estimated_sum).abs
+
+  puts
+  puts sim.class.to_s
+  puts sim.variant_description
+  puts "total_clicks: #{sim.total_clicks}"
+  puts "actual sum: #{sim.actual_sum}"
+  puts "absolute deviation: #{deviation}"
+  puts "fraction deviation: #{deviation / sim.actual_sum}"
+end
--- lib/ui/full_keypad.rb
+++ lib/ui/full_keypad.rb
@@ -1,25 +1,33 @@
 class FullKeypad
-  def initialize(number_list)
+  def self.run_simulations(number_list)
+    variants = [
+      self.new("user must enter decimal point", number_list, require_decimal_point: false),
+      self.new("user skips decimal point", number_list)
+    ]
+    variants.each(&:simulate)
+    variants
+  end
+
+  attr_reader :variant_description,
+              :total_clicks,
+              :estimated_sum,
+              :actual_sum
+
+  def initialize(variant_description, number_list, options = {})
     @numbers = number_list.numbers
     @actual_sum = number_list.tally
+    @variant_description = variant_description
+    @options = options
   end
 
-  def simulate(require_decimal_point: true)
+  def simulate
     @estimated_sum = @actual_sum
-    @total_clicks = @numbers.map {|number| digits(number, require_decimal_point) }.reduce(&:+)
-  end
-
-  def total_clicks
-    @total_clicks
-  end
-
-  def estimated_sum
-    @estimated_sum
+    @total_clicks = @numbers.map {|number| digits(number, @options) }.reduce(&:+)
   end
 
   private
 
-  def digits(number, require_decimal_point)
+  def digits(number, require_decimal_point: true)
     string =
       if require_decimal_point
         number.to_s
